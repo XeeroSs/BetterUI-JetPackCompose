1cb677bff77bf2deeeb249f2dfb70fec70165cf7
XeeroSs <qmasini@gmail.com>
2025-02-08 21:17:14 +0100

    JitPack.yml supprimé et  balise <application> du manifest retiré

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.XeeroSs"
                 artifactId = "BetterUI-JetPackCompose"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
