dfec0622fb385c54aa9c743c5e1f78cd5f8a6553
XeeroSs <qmasini@gmail.com>
2025-02-12 11:12:23 +0100

    up to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.XeeroSs"
                 artifactId = "BetterUI-JetPackCompose"
-                version = "1.2.0"
+                version = "1.3.0"
             }
         }
     }
